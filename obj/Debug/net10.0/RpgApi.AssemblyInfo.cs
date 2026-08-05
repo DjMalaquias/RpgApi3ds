13069f3025826584f7e1c2605d5208381d3fdf38
DjMalaquias <vag.salsa@gmail.com>
2026-08-04 22:11:36 -0300

    RpgApiAzureConected

--- obj/Debug/net10.0/RpgApi.AssemblyInfo.cs
+++ obj/Debug/net10.0/RpgApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RpgApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d85bb3dcf8bb9737799c5e279275fc87cb5a481")]
 [assembly: System.Reflection.AssemblyProductAttribute("RpgApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RpgApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
